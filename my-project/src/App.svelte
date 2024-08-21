cd2f867374f243a723a9d9b7a82ca85ca0d03ba8
Manuel Coronel <mcoronel814@hotmail.com>
2024-08-21 16:07:49 -0400

    feat: Update home section with profile image
    This commit adds a profile image to the home section of the website. The image is displayed using the `img` tag with the source set to "/profmev2.jpg". This image enhances the visual appeal of the website and provides a visual representation of the website owner, Manuel Coronel.

--- my-project/src/App.svelte
+++ my-project/src/App.svelte
@@ -10,6 +10,7 @@
 
 <section id="home" class="min-h-screen flex items-center justify-center bg-[var(--bg-color)] text-center p-5">
   <div>
+    <img src="/profmev2.jpg" alt="Manuel Coronel" class="rounded-full w-96 h-96 mx-auto mb-4" />
     <h1 class="text-4xl sm:text-5xl font-bold text-[var(--highlight-color)]">Manuel Coronel</h1>
     <p class="text-lg sm:text-xl mt-4 text-[var(--text-color)]">Security Analyst | Web Developer | Fitness Enthusiast</p>
   </div>
